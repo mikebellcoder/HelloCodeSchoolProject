3124c6900b3abfe6282107e8a0e358681fe7b6e8
ngMike <mikebell@scientology.net>
2016-12-16 21:23:03 -0800

    Update index_test.js

--- test/index_test.js
+++ test/index_test.js
@@ -18,32 +18,32 @@ describe('Your HTML Page', function() {
   });
 
   it('should have a title @title', function() {
-    assert.equal($('title').length, 1, 'Make sure to create a `title` element.');
+    assert.equal(window.$('title').length, 1, 'Make sure to create a `title` element.');
   });
 
   it('should have a title that contains your name @title', function() {
-    assert.notEqual($('title').text(), '', 'Make sure to set the content of the `title` element to your Code School username.');
+    assert.notEqual(window.$('title').text(), '', 'Make sure to set the content of the `title` element to your Code School username.');
   });
 
   it('should have a h1 element @h1', function() {
-    assert.isAtLeast($('h1').length, 1, "Make sure to create an `h1` element.");
+    assert.isAtLeast(window.$('h1').length, 1, "Make sure to create an `h1` element.");
   });
 
   it('should have content in the h1 element @h1', function() {
-    assert.equal($('h1').text(), 'Hello, Code School!', "Make sure to set the content of your `h1` element to 'Hello, Code School!'.");
+    assert.equal(window.$('h1').text(), 'Hello, Code School!', "Make sure to set the content of your `h1` element to 'Hello, Code School!'.");
   });
 
   it('should have a ul @ul', function() {
-    assert.isAtLeast($('ul').length, 1, "Make sure to create a `ul` element.");
+    assert.isAtLeast(window.$('ul').length, 1, "Make sure to create a `ul` element.");
   });
 
   it('should have at least 2 li elements @li', function() {
-    assert.isAtLeast($('li').length, 2, "Make sure to create at least 2 `li` elements.");
+    assert.isAtLeast(window.$('li').length, 2, "Make sure to create at least 2 `li` elements.");
   });
 
   it('should have content for all `li` elements. @li', function() {
     var message = "Make sure to include something you want to learn for each `li` element."
-    assert.notEqual($('li:first').text(), '', message);
-    assert.notEqual($('li:last').text(), '', message);
+    assert.notEqual(window.$('li:first').text(), '', message);
+    assert.notEqual(window.$('li:last').text(), '', message);
   });
 });
